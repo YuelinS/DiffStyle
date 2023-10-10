dce15ac5700be04aa20fb88dd3638322d21b41fa
Yuelin Shi <ysshi@caltech.edu>
2023-10-10 12:58:45 -0700

    Initial commit

---
+++ bash/exp_modify.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python cartoon_modify.py
---
+++ bash/exp_styles.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0 python cartoon_styles.py --description "A handsome man in short hair"  --path example_images/portrait5.jpg --self_ratio 0.2 --blend man
+CUDA_VISIBLE_DEVICES=0 python cartoon_styles.py --description "A handsome man in short hair"  --path example_images/portrait5.jpg --self_ratio 0.3 --blend man
+CUDA_VISIBLE_DEVICES=0 python cartoon_styles.py --description "A handsome man in short hair"  --path example_images/portrait5.jpg --self_ratio 0.4 --blend man
+CUDA_VISIBLE_DEVICES=0 python cartoon_styles.py --description "A handsome man in short hair"  --path example_images/portrait5.jpg --self_ratio 0.5 --blend man
+CUDA_VISIBLE_DEVICES=0 python cartoon_styles.py --description "A handsome man in short hair"  --path example_images/portrait5.jpg --self_ratio 0.6 --blend man
+CUDA_VISIBLE_DEVICES=0 python cartoon_styles.py --description "A handsome man in short hair"  --path example_images/portrait5.jpg --self_ratio 0.7 --blend man
+CUDA_VISIBLE_DEVICES=0 python cartoon_styles.py --description "A handsome man in short hair"  --path example_images/portrait5.jpg --self_ratio 0.8 --blend man
+CUDA_VISIBLE_DEVICES=2 python cartoon_styles.py --description "A serious man in short hair"  --path example_images/portrait4.jpg --self_ratio 0.2 --blend man
+CUDA_VISIBLE_DEVICES=2 python cartoon_styles.py --description "A serious man in short hair"  --path example_images/portrait4.jpg --self_ratio 0.3 --blend man
+CUDA_VISIBLE_DEVICES=2 python cartoon_styles.py --description "A serious man in short hair"  --path example_images/portrait4.jpg --self_ratio 0.4 --blend man
+CUDA_VISIBLE_DEVICES=2 python cartoon_styles.py --description "A serious man in short hair"  --path example_images/portrait4.jpg --self_ratio 0.5 --blend man
+CUDA_VISIBLE_DEVICES=2 python cartoon_styles.py --description "A serious man in short hair"  --path example_images/portrait4.jpg --self_ratio 0.6 --blend man
+CUDA_VISIBLE_DEVICES=2 python cartoon_styles.py --description "A serious man in short hair"  --path example_images/portrait4.jpg --self_ratio 0.7 --blend man
+CUDA_VISIBLE_DEVICES=2 python cartoon_styles.py --description "A serious man in short hair"  --path example_images/portrait4.jpg --self_ratio 0.8 --blend man
